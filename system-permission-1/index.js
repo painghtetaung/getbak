b90418a60d24b561acc52c5506f29f568d756e84
painghtetaung <painghtetaung1999@gmail.com>
2021-09-23 20:44:54 +0630

    finished

--- system-permission-1/index.js
+++ system-permission-1/index.js
@@ -1,6 +1,12 @@
 document.getElementById('add_team').onclick = changeColor;
 const add_icon = document.querySelector('#add_icon');
-var click = 0;
+var click = 1;
+if (click === 1) {
+    document.querySelector('.bg-modal').style.display = "flex";
+    document.getElementById('add_team').style.color = '#0055FF';
+    document.getElementById('span_team').style.color = '#0055FF';
+    add_icon.src = "../resources/Icon _ Fonts/uicons-regular-rounded/svg/fi-rr-add-blue.svg"
+}
 
 function changeColor() {
     click++;
